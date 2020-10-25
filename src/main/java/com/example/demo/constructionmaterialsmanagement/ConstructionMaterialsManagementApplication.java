3d65f6680aa920bbe1c1b1f3feeebf96ad9862c4
Trung <thanhtrung028473@gmail.com>
2020-10-25 19:48:03 +0700

    tao project spring boot

---
+++ src/main/java/com/example/demo/constructionmaterialsmanagement/ConstructionMaterialsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.constructionmaterialsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionMaterialsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConstructionMaterialsManagementApplication.class, args);
+    }
+
+}
